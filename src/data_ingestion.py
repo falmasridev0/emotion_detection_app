8945fc7d2eeaf1030d3ac294d630e24d232b130b
Faisal Almasri <falmasri.ai@gmail.com>
2025-04-15 13:02:14 +0300

    preprocessing phase

---
+++ src/data_ingestion.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from urllib.request import urlretrieve
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+# In[2]:
+
+
+#step 3.1 - load data from the remote source
+urlretrieve(url="https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv",
+                       filename="dataset.csv")
+
+
+# In[ ]:
+
+
+dataset = pd.read_csv("../data/raw/dataset.csv")
+
+
+# In[4]:
+
+
+#inspecting dataset:
+dataset.head()
+
+
+# In[5]:
+
+
+#remove tweet_id col:
+dataset = dataset.drop(columns="tweet_id")
+dataset
+
+
+# In[6]:
+
+
+# check classes:
+dataset['sentiment'].unique()
+
+
+# In[7]:
+
+
+#check number of samples for each class:
+dataset['sentiment'].value_counts()
+
+
+# In[8]:
+
+
+classes = ["sadness","happiness"]
+
+
+# In[9]:
+
+
+modified_dataset = dataset.loc[dataset['sentiment'].isin(classes)]
+
+
+# In[10]:
+
+
+#verify:
+modified_dataset
+
+
+# In[11]:
+
+
+#map classes (1 for happiness - 0 for sadness)
+modified_dataset['sentiment'] = modified_dataset.loc[:,'sentiment'].apply(lambda x: classes.index(x))
+
+
+# In[12]:
+
+
+modified_dataset
+
+
+# In[13]:
+
+
+train_set,valid_set = train_test_split(modified_dataset,test_size=0.3)
+valid_set,test_set = train_test_split(valid_set,test_size=0.5)
+
+
+# In[14]:
+
+
+#reset indices positioning:
+datasets = {
+    "train":train_set,
+    "valid":valid_set,
+    "test":test_set
+}
+
+
+# In[15]:
+
+
+for set in datasets:
+    print(f"number of samples in {set}_set is: {len(datasets[set])}")
+
+
+# In[16]:
+
+
+datasets
+
+
+# In[17]:
+
+
+for set in datasets:
+    datasets[set] = datasets[set].reset_index(drop=True)
+
+
+# In[ ]:
+
+
+# last step in data ingestion:
+for set in datasets:
+    datasets[set].to_csv(f"../data/data_preparation_phase/{set}_set.csv",index=False)
+
+
+# In[ ]:
+
+
+
+
---
+++ src/data_preprocessing.py
@@ -0,0 +1,72 @@
+import nltk
+import pandas as pd
+import os.path as path
+import re
+from nltk.corpus import stopwords
+
+SETS_PATH = "data/data_preparation_phase"
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+#step 1: data exploration:
+datasets = {'train':pd.read_csv(path.join(SETS_PATH,"train_set.csv")),
+            'valid':pd.read_csv(path.join(SETS_PATH,"valid_set.csv")),
+            'test':pd.read_csv(path.join(SETS_PATH,"test_set.csv"))}
+
+
+#step 2: pre_processing pipeline:
+
+def lower_case(text):
+    return text.lower()
+
+#why we did that? since emotions(happiness,sadness) are not usually correlated with this kind of emotions, likewise, symbols like !? could be used to represent angriness suspesion
+def remove_puncitions(text):
+    return re.sub(r'[^\w\s]',' ',text)
+
+#numbers will not contribute to the emotions usually unless something like "I waited 30 years to get that. finally!"
+def remove_numbers(text):
+    return re.sub(r'[0-9]','',text)
+
+#just did it eariler for tokenization later
+def trim_extra_spaces(text):
+    return re.sub(r'\s+', ' ', text).strip()
+
+
+def remove_stopwords(text,stopwords=stopwords.words('english')):
+    return " ".join(word for word in text.split(" ") if word not in stopwords)
+
+def remove_links(text):
+    return re.sub(r'\b(?:https?://|www\.)?\S+\.\S+\b', '', text)
+
+def remove_repitive_patterns(text):
+    # Step 1: Replace repeated characters (3+ times)
+    text = re.sub(r'(.)\1{2,}', r'\1', text)
+
+    # Step 2: Replace repeated words (2+ times)
+    text = re.sub(r'\b(\w+)(?:\s+\1)+\b', r'\1', text)
+
+    # Step 3: Replace repeated patterns (2+ times)
+    text = re.sub(r'\b(.+?)\1+\b', r'\1', text)
+
+    return text
+
+
+def preprocessing(content,functions):
+    for fun in functions:
+        content = fun(content)
+    return content
+
+
+preprocessing_functions = [lower_case,remove_puncitions,remove_numbers,trim_extra_spaces,remove_stopwords,remove_links,remove_repittive_patterns,nltk.casual.casual_tokenize]
+
+
+#let's tokenize all the sets and save them:
+for s in datasets:
+   datasets[s]['content'] = datasets[s]['content'].apply(lambda x: preprocessing(x,preprocessing_functions))
+
+
+#let's check some samples of each and save results:d
+for s in datasets:
+    print(f"{s}_set:\n{datasets[s].head()}")
+    datasets[s].to_csv(f"data/preprocessing_phase/{s}_set.csv")
+
